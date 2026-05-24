09290e5830bbf84da2e397ba52005e4ccc46e2a1
jmz <jmz@ordix.de>
2026-05-25 00:51:17 +0200

    added new functions for controller

--- api/v1alpha1/rediscluster_types.go
+++ api/v1alpha1/rediscluster_types.go
@@ -17,11 +17,56 @@ limitations under the License.
 package v1alpha1
 
 import (
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// Common Kubernetes configuration for all components of the Redis cluster.
+// +k8s:deepcopy-gen=true
+type KubernetesConfig struct {
+	Image                              string                               `json:"image,omitempty"`
+	ImagePullPolicy                    corev1.PullPolicy                    `json:"imagePullPolicy,omitempty"`
+	Resources                          corev1.ResourceRequirements          `json:"resources,omitempty"`
+	ImagePullSecrets                   []corev1.LocalObjectReference        `json:"imagePullSecrets,omitempty"`
+	UpdateStrategy                     appsv1.StatefulSetUpdateStrategy     `json:"updateStrategy,omitempty"`
+	PersistentVolumeClaimReclaimPolicy corev1.PersistentVolumeReclaimPolicy `json:"persistentVolumeClaimReclaimPolicy,omitempty"`
+	Service                            *ServiceConfig                       `json:"service,omitempty"`
+	IgnoreAnnotations                  []string                             `json:"ignoreAnnotations,omitempty"`
+	MinReadySeconds                    int32                                `json:"minReadySeconds,omitempty"`
+}
+
+// ServiceConfig define the type of service to be created and its annotations
+// +k8s:deepcopy-gen=true
+type ServiceConfig struct {
+	// +kubebuilder:validation:Enum=LoadBalancer;NodePort;ClusterIP
+	ServiceType        string            `json:"serviceType,omitempty"`
+	ServiceAnnotations map[string]string `json:"annotations,omitempty"`
+	// IncludeBusPort when set to true, it will add bus port to the service, such as 16379.
+	// This field is only used for Redis cluster mode.
+	IncludeBusPort *bool `json:"includeBusPort,omitempty"`
+	// Headless config for which suffix is -headless service
+	Headless *Service `json:"headless,omitempty"`
+	// Additional config for which suffix is -additional service
+	Additional *Service `json:"additional,omitempty"`
+}
+
+// Service is the struct to define the service type and its annotations
+// +k8s:deepcopy-gen=true
+type Service struct {
+	// +kubebuilder:validation:Enum=LoadBalancer;NodePort;ClusterIP
+	// +kubebuilder:default:=ClusterIP
+	Type                  string            `json:"type,omitempty"`
+	AdditionalAnnotations map[string]string `json:"additionalAnnotations,omitempty"`
+	// IncludeBusPort when set to true, it will add bus port to the service, such as 16379.
+	// This field is only used for Redis cluster mode.
+	IncludeBusPort *bool `json:"includeBusPort,omitempty"`
+	// +kubebuilder:default:=true
+	Enabled *bool `json:"enabled,omitempty"`
+}
 
 // RedisClusterSpec defines the desired state of RedisCluster.
 type RedisClusterSpec struct {
@@ -29,7 +74,12 @@ type RedisClusterSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of RedisCluster. Edit rediscluster_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	ClusterSize      *int32            `json:"clusterSize,omitempty"`
+	KubernetesConfig *KubernetesConfig `json:"kubernetesConfig,omitempty"`
+	HostNetwork      *bool             `json:"hostNetwork,omitempty"`
+	Port             *int32            `json:"port,omitempty"`
+	ClusterVersion   *string           `json:"clusterVersion,omitempty"`
+	RedisLeader
 }
 
 // RedisClusterStatus defines the observed state of RedisCluster.
